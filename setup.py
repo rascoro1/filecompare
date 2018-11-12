c1d86aff8ccb13ce8b2fb2268de8d538fb3016fd
rascoro1 <andcope1995@gmail.com>
2018-11-12 00:03:55 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="filecompare",
-    version="0.0.1",
+
+setup(
+    name = 'filecompare',
+    version = '0.1.1',
     author="rascoro1",
-    author_email="andcope1995@gmail.com",
-    description="commnad line utility for comparing files in directories",
+    author_email="andcope1995",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rascoro1/filecompare",
-    packages=setuptools.find_packages(),
+    packages = ['filecompare'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-)
+    entry_points = {
+        'console_scripts': [
+            'filecompare = filecompare.__main__:main'
+        ]
+    })
