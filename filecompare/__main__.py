fb0ca5c6b0eb7d47b61d1f133dc10366a5df0a62
rascoro1 <andcope1995@gmail.com>
2018-11-12 01:50:26 -0500

    functions

--- filecompare/__main__.py
+++ filecompare/__main__.py
@@ -11,17 +11,14 @@ def parse_args():
     parser.add_argument('directory', type=str, help='dir to search, for all similar files')
     return parser.parse_args()
 
-def main():
-    args = parse_args()
+def get_duplicate_hashes(dirname, hash_algo):
     hashes = {}
     duplicate_hashes = {}
-    dirname = args.directory.rstrip("/")
 
-    # Get all files in all sub directories
     for dir, dirname, filename in os.walk(dirname):
         for f in filename:
             file_path = dir + "/" + f
-            hash = hash_func(args.hash, file_path)
+            hash = hash_func(hash_algo, file_path)
             if hash in hashes:
                 # sort the array smallest first
                 if len(file_path) < len(hashes[hash][0]):
@@ -31,36 +28,54 @@ def main():
                 duplicate_hashes[hash] = hashes[hash]
             else:
                 hashes[hash] = [file_path]
-
+    
     del hashes
+    return duplicate_hashes
 
-    # Display duplicate hashes
+def pprint_hashes(duplicate_hashes):
     for hash, paths in duplicate_hashes.items():
-        if args.raw:
-            print("{}".format(",".join(paths)))
-        else:
-            print("{}: {}".format(len(paths), "\n\t-".join(paths)))
+        print("{}: {}".format(len(paths), "\n\t-".join(paths)))
 
+def raw_hashes(duplicate_hashes):
+    for hash, paths in duplicate_hashes.items():
+        print("{}".format(",".join(paths)))
+
+def delete_files(duplicate_hashes):
+    for hash, paths in duplicate_hashes.items():
+        print("##################################")
+        i = 0
+        for path in paths:
+            print("[{}]: {}".format(i, path))
+            i += 1
+
+        excluded_files = input("excluded files[0]: ")
+        if excluded_files == "":
+            excluded_files = "0"
+        excluded_files = excluded_files.split(" ")
+
+        i = 0
+        for path in paths:
+            if str(i) not in excluded_files:
+                os.remove(path)
+                print("removed {}".format(path))
+            i += 1
+
+def main():
+    args = parse_args()
+    dirname = args.directory.rstrip("/")
+
+    # get all duplicate files
+    duplicate_hashes = get_duplicate_hashes(dirname, args.hash)
+
+    # display duplicate files
+    if args.raw:
+        raw_hashes(duplicate_hashes)
+    else:
+        pprint_hashes(duplicate_hashes)
+
+    # --delete flag selected
     if args.delete:
-        for hash, paths in duplicate_hashes.items():
-            i = 0
-            print("##################################")
-            for path in paths:
-                print("[{}]: {}".format(i, path))
-                i += 1
-            excluded_files = input("excluded files[0]: ")
-            if excluded_files == "":
-                excluded_files = "0"
-            excluded_files = excluded_files.split(" ")
-            print("exclused files: {}".format(excluded_files))
-            i = 0
-            for path in paths:
-                if str(i) not in excluded_files:
-                    os.remove(path)
-                    print("removed {}".format(path))
-                i += 1
-
-
+        delete_files(duplicate_hashes)
 
 
 
